9f8e5d4ca366f87089df58837f4506c1e038b1e6
mandara11 <mandaram2345@gmail.com>
2022-09-24 20:43:27 +0530

    Added:UC1_AsyncofJS

--- AsyncDemo.js
+++ AsyncDemo.js
@@ -1,1 +1,14 @@
-console.log("Wellcome to the Ajax–Async, Callback & Promise");
+function showTime()
+{
+    const date=new Date();
+    return date.getHours()+" Hrs: "+date.getMinutes()+"Mins : "+date.getSeconds()+"Secs: ";
+}
+
+function showSessionExpire()
+{
+    console.log("Activity- B: your session expired at "+showTime());
+}
+
+console.log("Activity - A :Trigerring Activity - B at "+showTime());
+setTimeout(showSessionExpire,5000);
+console.log("Activity - A :Trigerring Activity - B "+showTime());
